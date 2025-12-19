fd21aff0714df20ea875bf861b48acd6fa828899
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2025-12-20 00:36:29 +0700

    fix: remove redundant attributes on derived record parameters to fix compilation

--- tools/Generator/Program.cs
+++ tools/Generator/Program.cs
@@ -260,16 +260,16 @@ namespace Generator
             // Base class parameters first
             var baseParams = new[]
             {
-                "[property: JsonPropertyName(\"messageId\")] System.Guid MessageId",
-                "[property: JsonPropertyName(\"messageName\")] string MessageName",
-                "[property: JsonPropertyName(\"messageType\")] MessageType MessageType",
-                "[property: JsonPropertyName(\"messageVersion\")] string MessageVersion",
-                "[property: JsonPropertyName(\"publishedBy\")] string PublishedBy",
-                "[property: JsonPropertyName(\"consumedBy\")] System.Collections.Generic.IReadOnlyList<string> ConsumedBy",
-                "[property: JsonPropertyName(\"correlationId\")] System.Guid CorrelationId",
-                "[property: JsonPropertyName(\"causationId\")] System.Guid? CausationId",
-                "[property: JsonPropertyName(\"occurredAtUtc\")] System.DateTimeOffset OccurredAtUtc",
-                "[property: JsonPropertyName(\"isPublic\")] bool IsPublic"
+                "System.Guid MessageId",
+                "string MessageName",
+                "MessageType MessageType",
+                "string MessageVersion",
+                "string PublishedBy",
+                "System.Collections.Generic.IReadOnlyList<string> ConsumedBy",
+                "System.Guid CorrelationId",
+                "System.Guid? CausationId",
+                "System.DateTimeOffset OccurredAtUtc",
+                "bool IsPublic"
             };
 
             var allParams = baseParams.ToList();
